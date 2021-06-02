fd3f5b0da26b77e7de97bb058be73e069cbf0cdd
krishnaneupaney <krishnaneupaney000@gmail.com>
2021-06-02 09:43:04 -0400

    mobile friendly

--- src/styles.js
+++ src/styles.js
@@ -15,4 +15,10 @@ export default makeStyles((theme) => ({
   image: {
     marginLeft: '15px',
   },
+  [theme.breakpoints.down('sm')]:{
+    mainContainer: {
+    flexDirection: "column-reverse"
+  }
+  }
+  
 }));
