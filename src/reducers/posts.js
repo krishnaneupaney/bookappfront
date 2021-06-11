7b30c964282a22dcf6331862e3b650aeb4587994
krishnaneupaney <krishnaneupaney000@gmail.com>
2021-06-11 14:23:35 -0400

    auth ready for deploy

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,11 +1,12 @@
-import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
+import { FETCH_ALL, CREATE, UPDATE, LIKE, DELETE } from '../constants/actionTypes';
 
-const Post = (posts = [], action) => {
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (posts = [], action) => {
   switch (action.type) {
     case FETCH_ALL:
       return action.payload;
-    // case LIKE:
-    //   return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
+    case LIKE:
+      return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
     case CREATE:
       return [...posts, action.payload];
     case UPDATE:
@@ -17,4 +18,3 @@ const Post = (posts = [], action) => {
   }
 };
 
-export default Post
